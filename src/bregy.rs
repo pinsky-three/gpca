67cbf7e9d69c3b5edc432f97f409f47d14f333de
Bregy Malpartida <bregy@minsky.cc>
2023-03-16 00:47:24 -0500

    feat: help is coming from gpt-4

--- src/bregy.rs
+++ src/bregy.rs
@@ -1,7 +1,12 @@
-use std::{collections::HashMap, hash::Hash};
+use std::{
+    collections::HashMap,
+    fmt::{Debug, Formatter, Result},
+    hash::Hash,
+};
 
-type HyperEdge<E> = (Box<[usize]>, E);
+type HyperEdge<E> = (Box<Vec<usize>>, E);
 
+#[derive(Clone)]
 pub struct LocalHyperGraph<const D: usize, N, E>
 where
     N: Clone + Sync + Send + Hash + Eq,
@@ -9,18 +14,103 @@ where
 {
     nodes: Box<[N; D]>,
     edges: HashMap<usize, HyperEdge<E>>,
-    // dynamic: LocalHyperGraphEdgeDynamic<N, E>,
 }
 
-// type LocalHyperGraphEdgeDynamic<N, E> = dyn Fn(&N, &[HyperEdge<E>], &[N]) -> N + Sync + Send;
+impl<const D: usize, N, E> LocalHyperGraph<D, N, E>
+where
+    N: Clone + Sync + Send + Hash + Eq,
+    E: Clone + Sync + Send + Eq + PartialEq + Hash + Sized,
+{
+    pub fn is_leaf_node(&self, node_id: usize) -> bool {
+        let count = self
+            .edges
+            .values()
+            .filter(|edge| edge.0.contains(&node_id))
+            .count();
+        count == 1
+    }
+}
 
-// type LocalHyperGraphEdgeDynamic<N, E> = dyn Fn(&N, &[(&[usize], E)], &[N]) -> N + Sync + Send;
-
+#[derive(Clone)]
 pub enum ComplexHyperGraph<const D: usize, N, E>
 where
     N: Clone + Sync + Send + Hash + Eq,
     E: Clone + Sync + Send + Eq + PartialEq + Hash + Sized,
 {
-    SubGraphs(Box<[Self]>),
+    SubGraphs(HashMap<HyperEdge<E>, Self>),
     HyperGraph(Box<LocalHyperGraph<D, N, E>>),
+}
+
+impl<const D: usize, N, E> ComplexHyperGraph<D, N, E>
+where
+    N: Clone + Sync + Send + Hash + Eq,
+    E: Clone + Sync + Send + Eq + PartialEq + Hash + Sized,
+{
+    pub fn if_leaf<F, R>(&self, f: F) -> Option<R>
+    where
+        F: FnOnce(&LocalHyperGraph<D, N, E>) -> R,
+    {
+        match self {
+            ComplexHyperGraph::SubGraphs(_) => None,
+            ComplexHyperGraph::HyperGraph(hg) => Some(f(hg)),
+        }
+    }
+}
+
+fn create_quadtree<const D: usize>(
+    depth: usize,
+    current_depth: usize,
+) -> ComplexHyperGraph<D, Vec<bool>, ()> {
+    if current_depth == depth {
+        let nodes_vec: Vec<Vec<bool>> = vec![vec![false; 1 << current_depth]; D];
+        let nodes: Box<[Vec<bool>; D]> = match nodes_vec.try_into() {
+            Ok(array) => Box::new(array),
+            Err(_) => panic!("Failed to convert Vec into Box<[Vec<bool>; D]>"),
+        };
+        ComplexHyperGraph::HyperGraph(Box::new(LocalHyperGraph {
+            nodes,
+            edges: HashMap::new(),
+        }))
+    } else {
+        let mut subgraphs = HashMap::new();
+        for _ in 0..4 {
+            let subgraph = create_quadtree::<D>(depth, current_depth + 1);
+            let edge = (Box::new(vec![0]), ());
+            subgraphs.insert(edge, subgraph);
+        }
+        ComplexHyperGraph::SubGraphs(subgraphs)
+    }
+}
+
+pub fn build_quadtree<const D: usize>(depth: usize) -> ComplexHyperGraph<D, Vec<bool>, ()> {
+    create_quadtree::<D>(depth, 0)
+}
+
+impl<const D: usize, N, E> Debug for LocalHyperGraph<D, N, E>
+where
+    N: Clone + Sync + Send + Hash + Eq + Debug,
+    E: Clone + Sync + Send + Eq + PartialEq + Hash + Sized + Debug,
+{
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+        f.debug_struct("LocalHyperGraph")
+            .field("nodes", &self.nodes)
+            .field("edges", &self.edges)
+            .finish()
+    }
+}
+
+impl<const D: usize, N, E> Debug for ComplexHyperGraph<D, N, E>
+where
+    N: Clone + Sync + Send + Hash + Eq + Debug,
+    E: Clone + Sync + Send + Eq + PartialEq + Hash + Sized + Debug,
+{
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+        match self {
+            ComplexHyperGraph::SubGraphs(subgraphs) => f
+                .debug_map()
+                .entries(subgraphs.iter().map(|(k, v)| (k, v)))
+                .finish(),
+            ComplexHyperGraph::HyperGraph(hg) => hg.fmt(f),
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ pub mod bregy;
 
 use std::collections::HashMap;
 
+use bregy::build_quadtree;
 use gpca::haca::HyperGraph;
 use image::{ImageBuffer, Rgb, RgbImage};
 
@@ -78,70 +79,74 @@ fn new_game_of_life_hyper_graph<const D: usize>(
 
 #[tokio::main]
 async fn main() {
-    const W: usize = 512;
-    const H: usize = W;
+    // const W: usize = 512;
+    // const H: usize = W;
 
-    let mut mem = box_array![LifeState::Dead; 262144];
+    // let mut mem = box_array![LifeState::Dead; 262144];
 
-    mem.par_iter_mut().for_each(|x| {
-        *x = if thread_rng().gen_bool(0.5) {
-            LifeState::Alive
-        } else {
-            LifeState::Dead
-        }
-    });
+    // mem.par_iter_mut().for_each(|x| {
+    //     *x = if thread_rng().gen_bool(0.5) {
+    //         LifeState::Alive
+    //     } else {
+    //         LifeState::Dead
+    //     }
+    // });
 
-    let mut graph = new_game_of_life_hyper_graph(mem);
+    // let mut graph = new_game_of_life_hyper_graph(mem);
 
-    // for _ in tqdm!(0..1000) {
-    //     graph
-    //         .classic_update_nodes_by_neighborhood(|current, neighborhood| {
-    //             let neighbors = neighborhood
-    //                 .iter()
-    //                 .filter(|x| **x == LifeState::Alive)
-    //                 .count();
-    //             if neighbors == 3 {
-    //                 LifeState::Alive
-    //             } else if neighbors == 2 {
-    //                 *current
-    //             } else {
-    //                 LifeState::Dead
-    //             }
-    //         })
-    //         .await;
+    // // for _ in tqdm!(0..1000) {
+    // //     graph
+    // //         .classic_update_nodes_by_neighborhood(|current, neighborhood| {
+    // //             let neighbors = neighborhood
+    // //                 .iter()
+    // //                 .filter(|x| **x == LifeState::Alive)
+    // //                 .count();
+    // //             if neighbors == 3 {
+    // //                 LifeState::Alive
+    // //             } else if neighbors == 2 {
+    // //                 *current
+    // //             } else {
+    // //                 LifeState::Dead
+    // //             }
+    // //         })
+    // //         .await;
+    // // }
+
+    // graph
+    //     .run_hashlife(1000, &|current, neighborhood| {
+    //         let neighbors = neighborhood
+    //             .iter()
+    //             .filter(|x| **x == LifeState::Alive)
+    //             .count();
+    //         if neighbors == 3 {
+    //             LifeState::Alive
+    //         } else if neighbors == 2 {
+    //             *current
+    //         } else {
+    //             LifeState::Dead
+    //         }
+    //     })
+    //     .await;
+
+    // let mut img: RgbImage = ImageBuffer::new(W as u32, H as u32);
+
+    // let copy_mem = graph.nodes();
+
+    // for y in 0..H {
+    //     for x in 0..W {
+    //         let index = (y * H) + x;
+
+    //         if copy_mem[index] == LifeState::Alive {
+    //             img.put_pixel(x as u32, y as u32, Rgb([255, 255, 255]));
+    //         } else {
+    //             img.put_pixel(x as u32, y as u32, Rgb([0, 0, 0]));
+    //         }
+    //     }
     // }
 
-    graph
-        .run_hashlife(1000, &|current, neighborhood| {
-            let neighbors = neighborhood
-                .iter()
-                .filter(|x| **x == LifeState::Alive)
-                .count();
-            if neighbors == 3 {
-                LifeState::Alive
-            } else if neighbors == 2 {
-                *current
-            } else {
-                LifeState::Dead
-            }
-        })
-        .await;
+    // img.save("hca_game_of_life_test.png").unwrap();
 
-    let mut img: RgbImage = ImageBuffer::new(W as u32, H as u32);
+    let quadtree = build_quadtree::<2>(3);
 
-    let copy_mem = graph.nodes();
-
-    for y in 0..H {
-        for x in 0..W {
-            let index = (y * H) + x;
-
-            if copy_mem[index] == LifeState::Alive {
-                img.put_pixel(x as u32, y as u32, Rgb([255, 255, 255]));
-            } else {
-                img.put_pixel(x as u32, y as u32, Rgb([0, 0, 0]));
-            }
-        }
-    }
-
-    img.save("hca_game_of_life_test.png").unwrap();
+    println!("{:?}", quadtree);
 }
